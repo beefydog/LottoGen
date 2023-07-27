b307d1d7f482d22f521991c2d0a2e059ac738693
beefydog <dave@beefydog.com>
2023-07-27 14:06:44 -0400

    Add project files.

---
+++ LottoGenWeb/Controls/BlockNumberGroup.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using LottoGenWeb.Models;
+
+namespace LottoGenWeb.Controls
+{
+    public partial class BlockNumberGroup
+    {
+        [Parameter]
+        public NumberGroup? ng { get; set; }
+
+        protected override void OnInitialized()
+        {
+        }
+
+        [Parameter]
+        public EventCallback<NumberGroup> OnItemChange { get; set; }
+
+        private async Task HandleChange()
+        {
+            await OnItemChange.InvokeAsync(ng);
+        }
+    }
+}
---
+++ LottoGenWeb/Models/NumberSetsRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace LottoGenWeb.Models;
+
+public class NumberGroupRequest
+{
+    public int min { get; set; }
+    public int max { get; set; }
+    public int numbersPerGroup { get; set; }
+    public int divergence { get; set; }
+    public bool sumCheck { get; set; }
+    public bool oeCheck { get; set; }
+}
+
+public class Root
+{
+    public List<NumberGroupRequest> numberSet { get; set; } = new List<NumberGroupRequest>();
+    public int sets { get; set; }
+}
