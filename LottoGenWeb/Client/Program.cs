9b4d7bd9a739aeb7ec65a6c0f9d287612c133e40
beefydog <dave@beefydog.com>
2024-07-03 14:15:49 -0400

    remove extraneous stuff - changed api endpoint to api  project enpoint

--- LottoGenWeb/Client/Program.cs
+++ LottoGenWeb/Client/Program.cs
@@ -11,8 +11,7 @@ public class Program
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-        builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(@"https://api.miraclecat.com/") });
+        builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(@"https://localhost:7138") });
 
         await builder.Build().RunAsync();
     }
